aa56ac4c8a539053e0de9bbc7f35ba020e267286
Subh0710 <93439739+Subh0710@users.noreply.github.com>
2022-09-08 15:17:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 st.header("UK Bank Dataset")
-df=pd.read_csv("C:\\Users\\Subham\\Desktop\\Datasets\\P1-UK-Bank-Customers.csv")
+df=pd.read_csv("P1-UK-Bank-Customers.csv")
 st.table(data=df)
